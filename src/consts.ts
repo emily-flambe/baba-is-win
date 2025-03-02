7f15b623eb3040dbd39c5e7572eb3d1b44cabd40
Flambé <emilycogsdill@gmail.com>
2025-03-01 20:17:01 -0700

    Revert "install react and MUI"
    This reverts commit 0f695c8912a9bbe8816db48f3326322a753f8ddc.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Baba is Blog";
-export const SITE_DESCRIPTION = "LOVE is WEAK. LOVE has KEY. KEY is OPEN.";
+export const SITE_TITLE = "Astro Blog";
+export const SITE_DESCRIPTION = "Welcome to my website!";
